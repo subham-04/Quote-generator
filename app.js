8d7626e6e528ef1917483c62d184890e9aea88cc
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2022-05-25 21:12:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText= document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
+const newQuoteBtn = document.getElementById("new-quote")
 
 
 let apiQuotes = [];
