51ee37c1df23203b256c018b0df096d2ad66fb35
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2022-05-25 20:23:01 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText= document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuote = document.getElementById("new-quote");
