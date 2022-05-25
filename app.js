f3d4867bf403261121c6280d0d84d5a57c2ec5a6
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2022-05-25 21:18:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText= document.getElementById("author");
