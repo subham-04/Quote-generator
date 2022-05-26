f15c22b63ced2de963f7ec7035f57558c1551ebd
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2022-05-26 10:00:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ const quoteText = document.getElementById("quote");
 const authorText= document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 
 let apiQuotes = [];
 
+// Start loading
+
+function loading(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+
+// Stop loading 
+
+function stopLoading(){
+    loader.hidden=true;
+    quoteContainer.hidden=false;
+}
+
 function newQuotes(){
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length )];
     // If the author is null then change it to unknown
     if(!quote.author){
@@ -25,10 +41,12 @@ function newQuotes(){
         quoteText.classList.remove("long-quote");
     }
     quoteText.textContent = quote.text;
+    stopLoading();
 }
 
 // get quotes from api
 async function getQuotes(){
+    loading();
     const apiUrl= 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
